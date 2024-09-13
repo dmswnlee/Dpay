6954c730f6efec992698cd2cdc16130a7d3ed507
LeeEunJoo <dlrmawnl03@gmail.com>
2024-09-13 20:45:10 +0900

    라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./UI/RootLayout.tsx";
+import Home from "./pages/Home.tsx";
+import Signup from "./pages/Signup.tsx";
+import Login from "./pages/Login.tsx";
+import CreateGroup from "./pages/CreateGroup.tsx";
+import ExpenseMain from "./pages/ExpenseMain.tsx";
+import MyPage from "./pages/MyPage.tsx";
+import NotFound from "./components/NotFound.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "signup", element: <Signup /> },
+			{ path: "login", element: <Login /> },
+			{ path: "create", element: <CreateGroup /> },
+			{ path: "expense", element: <ExpenseMain /> },
+			{ path: "mypage", element: <MyPage /> },
+			{ path: "*", element: <NotFound /> },
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
