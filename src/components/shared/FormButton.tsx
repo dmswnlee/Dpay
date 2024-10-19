20e51a359c9ceb3d9db6e6048a314a93831506ef
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-19 23:05:18 +0900

    공통 컴포넌트 FormButton 생성

--- src/components/shared/FormButton.tsx
+++ src/components/shared/FormButton.tsx
@@ -1,7 +1,23 @@
-const FormButton = () => {
-  return (
-    <div>FormButton</div>
-  )
+import styled from "styled-components";
+
+interface FormButtonProps {
+	name: string;
 }
 
-export default FormButton
+const FormButton = ({ name }: FormButtonProps) => {
+	return <StyledButton>{name}</StyledButton>;
+};
+
+export default FormButton;
+
+const StyledButton = styled.button`
+	outline: none;
+  border: none;
+	width: 100%;
+	height: 60px;
+	background-color: #3d8bfd;
+	border-radius: 5px;
+  color: #ffffff;
+  font-size: 18px;
+  cursor: pointer;
+`;
