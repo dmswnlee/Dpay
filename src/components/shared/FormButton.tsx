6fae9d945a6fda2f435bac6d5a9286198ae850fc
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-20 21:55:34 +0900

    호버 시 버튼 스타일링 추가

--- src/components/shared/FormButton.tsx
+++ src/components/shared/FormButton.tsx
@@ -12,12 +12,15 @@ export default FormButton;
 
 const StyledButton = styled.button`
 	outline: none;
-  border: none;
+	border: none;
 	width: 100%;
 	height: 60px;
 	background-color: #3d8bfd;
 	border-radius: 5px;
-  color: #ffffff;
-  font-size: 18px;
-  cursor: pointer;
+	color: #ffffff;
+	font-size: 18px;
+	cursor: pointer;
+	&:hover {
+		filter: brightness(0.8);
+	}
 `;
