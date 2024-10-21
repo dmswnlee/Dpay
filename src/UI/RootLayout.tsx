7d5b26471bb295849f2f32cd55b582388f721325
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-21 22:30:46 +0900

    ChakraProvider추가 및 수정

--- src/UI/RootLayout.tsx
+++ src/UI/RootLayout.tsx
@@ -1,15 +1,28 @@
-import { Outlet } from 'react-router-dom'
-import Header from '../components/Header'
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#F8F9FA",
+      },
+    },
+  },
+});
 
 const RootLayout = () => {
-  return (
-    <section>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </section>
-  )
-}
+	return (
+		<ChakraProvider theme={theme} resetCSS={false}>
+			<section>
+				<Header />
+				<main>
+					<Outlet />
+				</main>
+			</section>
+		</ChakraProvider>
+	);
+};
 
-export default RootLayout
+export default RootLayout;
