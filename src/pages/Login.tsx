ad535e2757e7e2b063a6e89baa3a7a901d06e589
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-22 14:41:16 +0900

    로그인 페이지 폼 구현 및 스타일링

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,100 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useForm } from "react-hook-form";
+import OverlayWrapper from "../components/shared/OverlayWrapper";
+import FormButton from "../components/shared/FormButton";
+import { StyledContainer, StyledFormWrapper, StyledInput, StyledInputWrapper, StyledLabel } from "./Signup";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
-export default Login
+const Login = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		mode: "onChange",
+	});
+
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
+
+	return (
+		<StyledContainer>
+			<OverlayWrapper minHeight="50vh">
+				<StyledLogoWrapper>
+					<StyledLogo src={logo} alt="로고" />
+				</StyledLogoWrapper>
+				<StyledForm onSubmit={handleSubmit(onSubmit)}>
+					<StyledFormWrapper>
+						<StyledInputWrapper>
+							<StyledLabel htmlFor="email">아이디</StyledLabel>
+							<StyledInput
+								id="email"
+								{...register("email", {
+									required: "아이디를 입력해주세요.",
+									pattern: {
+										value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+										message: "유효한 이메일 주소를 입력해주세요.",
+									},
+									maxLength: {
+										value: 20,
+										message: "이름은 최대 20글자까지 입력 가능합니다.",
+									},
+								})}
+								placeholder="아이디를 입력해주세요."
+							/>
+							{errors.email && <span>{errors.email.message}</span>}
+						</StyledInputWrapper>
+						<StyledInputWrapper>
+							<StyledLabel htmlFor="password">비밀번호</StyledLabel>
+							<StyledInput
+								id="password"
+								{...register("password", {
+									required: "비밀번호를 입력해주세요.",
+									maxLength: {
+										value: 15,
+										message: "비밀번호는 최대 15글자까지 입력 가능합니다.",
+									},
+								})}
+								placeholder="비밀번호를 입력해주세요."
+							/>
+							{errors.password && <span>{errors.password.message}</span>}
+						</StyledInputWrapper>
+
+            <StyledButton>아직 회원가입 전 이신가요?</StyledButton>
+					</StyledFormWrapper>
+
+					<FormButton text="로그인" />
+				</StyledForm>
+			</OverlayWrapper>
+		</StyledContainer>
+	);
+};
+
+export default Login;
+
+const StyledForm = styled.form`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+`;
+
+const StyledButton = styled.button`
+	border: none;
+	background-color: transparent;
+	font-size: 16px;
+	color: #a0aec0;
+	cursor: pointer;
+`;
+
+const StyledLogoWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+  margin-bottom: 20px;
+`;
+
+const StyledLogo = styled.img`
+	width: 94px;
+	height: 46px;
+`;
