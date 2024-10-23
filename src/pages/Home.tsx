3ffe7b8355f869c2c214d2a6530e9f069c318be1
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-23 12:39:44 +0900

    그룹 생성하기 버튼 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,34 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import styled from "styled-components";
+import { FiPlus } from "react-icons/fi";
+import CardOverlay from "../components/shared/CardOverlay";
 
-export default Home
+const Home = () => {
+	return (
+		<StyledHomeContainer>
+			<StyledHomeWrapper>
+				<CardOverlay>
+					<FiPlus />
+					<StyledAddButton>그룹 생성하기</StyledAddButton>
+				</CardOverlay>
+			</StyledHomeWrapper>
+		</StyledHomeContainer>
+	);
+};
+
+export default Home;
+
+const StyledHomeContainer = styled.div`
+	display: flex;
+	justify-content: center;
+  margin-top: 70px;
+`;
+
+const StyledHomeWrapper = styled.div`
+	width: 80%;
+`;
+
+const StyledAddButton = styled.button`
+	border: none;
+	background-color: transparent;
+	font-size: 20px;
+`;
