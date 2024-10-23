a2066efcadcecdb65e9b813781d3c67ff80926d9
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-23 22:01:46 +0900

    테스트 충돌 해결

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
 	},
 	moduleNameMapper: {
 		"\\.(css|less|sass|scss)$": "identity-obj-proxy",
+		"\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
 	},
 	testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
 };
