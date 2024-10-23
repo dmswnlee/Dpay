a2521af441112681881c744d0403c8192079f9e5
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-23 11:55:51 +0900

    로그인 전 홈페이지 실패하는 테스트 케이스 작성

---
+++ src/pages/Home.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Home from "./Home";
+import userEvent from "@testing-library/user-event";
+
+const renderComponent = () => {
+	render(
+		<MemoryRouter>
+			<Home />
+		</MemoryRouter>,
+	);
+
+	const addButton = screen.getByRole("button", { name: /그룹 생성하기/i });
+
+	return {
+		addButton,
+	};
+};
+
+describe("Home", () => {
+	test("그룹 생성하기 버튼 클릭 시 그룹 생성 페이지로 이동", () => {
+		const { addButton } = renderComponent();
+
+		userEvent.click(addButton);
+	});
+});
