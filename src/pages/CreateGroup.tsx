877e7bf56b993a882c5590ae3b2b846861aa4bc5
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-24 15:48:02 +0900

    모임 생성 페이지 스타일링

--- src/pages/CreateGroup.tsx
+++ src/pages/CreateGroup.tsx
@@ -1,7 +1,34 @@
-const CreateGroup = () => {
-  return (
-    <div>CreateGroup</div>
-  )
-}
+import FormButton from '../components/shared/FormButton';
+import OverlayWrapper from "../components/shared/OverlayWrapper";
+import { StyledContainer, StyledForm, StyledFormWrapper, StyledInput, StyledInputWrapper, StyledLabel } from "./Signup";
 
-export default CreateGroup
+const CreateGroup = () => {
+	return (
+		<StyledContainer>
+			<OverlayWrapper minHeight="50vh">
+				<StyledForm>
+					<StyledFormWrapper>
+						<StyledInputWrapper>
+							<StyledLabel>당신의 모임 이름은 무엇인가요?</StyledLabel>
+							<StyledInput />
+						</StyledInputWrapper>
+
+						<StyledInputWrapper>
+							<StyledLabel>모임의 멤버를 입력해주세요.</StyledLabel>
+							<StyledInput />
+						</StyledInputWrapper>
+
+						<StyledInputWrapper>
+							<StyledLabel>모임 날짜를 입력해주세요.</StyledLabel>
+							<StyledInput />
+						</StyledInputWrapper>
+					</StyledFormWrapper>
+
+          <FormButton text="생성하기" />
+				</StyledForm>
+			</OverlayWrapper>
+		</StyledContainer>
+	);
+};
+
+export default CreateGroup;
