bdb2e6375dfbda596921c30fbf3b6d69255cf54b
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-25 14:10:52 +0900

    비용 입력 페이지 레이아웃

--- src/pages/ExpenseMain.tsx
+++ src/pages/ExpenseMain.tsx
@@ -1,7 +1,35 @@
-const ExpenseMain = () => {
-  return (
-    <div>ExpenseMain</div>
-  )
-}
+import styled from "styled-components";
+import AddExpenseForm from "../components/AddExpenseForm";
+import ExpenseList from "../components/ExpenseList";
+import SettlementSummary from "../components/SettlementSummary";
 
-export default ExpenseMain
+const ExpenseMain = () => {
+	return (
+		<StyledExpenseContainer>
+			<StyledExpenseWrapper>
+				<div>
+					<AddExpenseForm />
+					<SettlementSummary />
+				</div>
+				<div>
+					<ExpenseList />
+				</div>
+			</StyledExpenseWrapper>
+		</StyledExpenseContainer>
+	);
+};
+
+export default ExpenseMain;
+
+const StyledExpenseContainer = styled.div`
+	width: 100vw;
+  display: flex;
+	justify-content: center;
+`;
+
+const StyledExpenseWrapper = styled.div`
+	width: 80%;
+	display: flex;
+	justify-content: space-between;
+  margin: 70px 0;
+`;
