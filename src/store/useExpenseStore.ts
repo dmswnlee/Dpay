1f9959087448cca0df7de718c7567ab791d4634c
LeeEunJoo <dlrmawnl03@gmail.com>
2024-10-27 16:53:48 +0900

    상태 추가

--- src/store/useExpenseStore.ts
+++ src/store/useExpenseStore.ts
@@ -1,20 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface Expense {
-  date: string;
-  desc: string;
-  memo: string;
-  amount: number;
-  member: string;
+	date: string;
+	desc: string;
+	memo: string;
+	amount: number;
+	member: string;
 }
 
 interface ExpenseState {
-  expenses: Expense[];
-  addExpense: (expense: Expense) => void;
+	expenses: Expense[];
+	addExpense: (expense: Expense) => void;
+	setExpenses: (expenses: Expense[]) => void;
 }
 
-export const useExpenseStore = create<ExpenseState>((set) => ({
-  expenses: [],
-  addExpense: (expense) =>
-    set((state) => ({ expenses: [...state.expenses, expense] })),
+export const useExpenseStore = create<ExpenseState>(set => ({
+	expenses: [],
+	addExpense: expense => set(state => ({ expenses: [...state.expenses, expense] })),
+	setExpenses: expenses => set({ expenses }),
 }));
