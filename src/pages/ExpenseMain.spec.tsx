ac82d1bea37e332c50b487e3adcb47a512f7aa09
LeeEunJoo <dlrmawnl03@gmail.com>
2024-11-01 17:20:52 +0900

    정산결과 실패하는 테스트 케이스 작성

--- src/pages/ExpenseMain.spec.tsx
+++ src/pages/ExpenseMain.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ExpenseMain from "./ExpenseMain";
 import userEvent from "@testing-library/user-event";
@@ -87,6 +87,70 @@ describe("ExpenseMain", () => {
 
 			const expenseListComponent = screen.getByTestId("expenseList");
 			expect(expenseListComponent).toBeInTheDocument();
+		});
+	});
+
+	describe("SettlementSummary", () => {
+		test("정산 결과 컴포넌트가 렌더링 되는가", () => {
+			renderComponent();
+
+			const component = screen.getByText(/정산 결과/i);
+			expect(component).toBeInTheDocument();
+		});
+	});
+
+	describe("새로운 비용이 입력 되었을 때", () => {
+		const addNewExpense = async () => {
+			const { dateInput, descInput, payerInput, amountInput, addButton } = renderComponent();
+			await userEvent.type(dateInput, "2024-11-01");
+			await userEvent.type(descInput, "장보기");
+			await userEvent.type(amountInput, "34000");
+			await userEvent.selectOptions(payerInput, "은주");
+			await userEvent.click(addButton);
+		};
+
+		beforeEach(async () => {
+			await addNewExpense();
+		});
+
+		test("날짜, 내용, 결제자, 금액 데이터가 정산 리스트에 추가 된다.", () => {
+			const expenseListComponent = screen.getByTestId("expenseList");
+
+			const dateValue = within(expenseListComponent).getByText("2024-11-01");
+			expect(dateValue).toBeInTheDocument();
+
+			const descValue = within(expenseListComponent).getByText("장보기");
+			expect(descValue).toBeInTheDocument();
+
+			const payerValue = within(expenseListComponent).getByText("은주");
+			expect(payerValue).toBeInTheDocument();
+
+			const amountValue = within(expenseListComponent).getByText("34000원");
+			expect(amountValue).toBeInTheDocument();
+		});
+
+		test("정산 결과 또한 업데이트가 된다.", () => {
+			const totalText = screen.getByText(/2 명이서 총 30000 원 지출/i);
+			expect(totalText).toBeInTheDocument();
+
+			const transactionText = screen.getByText(/영희가 영수에게 15000원 보내기/i);
+			expect(transactionText).toBeInTheDocument();
+		});
+
+		const htmlToImage = require("html-to-image");
+
+		test("정산 결과를 이미지 파일로 저장할 수 있다.", async () => {
+			const spiedToPng = jest.spyOn(htmlToImage, "toPng");
+
+			const downloadBtn = screen.getByTestId("btn-download");
+			await waitFor(() => {
+				expect(downloadBtn).toBeInTheDocument();
+			});
+
+			userEvent.click(downloadBtn);
+			await waitFor(() => {
+				expect(spiedToPng).toHaveBeenCalledTimes(1);
+			});
 		});
 	});
 });
